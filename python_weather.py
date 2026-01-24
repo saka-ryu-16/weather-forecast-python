897ddecfa27f7f1dc97773be535224aa0dbdaa7c
saka-ryu-16 <sr.04.ma@gmail.com>
2026-01-24 22:58:56 +0900

    都道府県選択機能を追加

--- python_weather.py
+++ python_weather.py
@@ -1,12 +1,80 @@
 import requests
 
+areas = {
+    "北海道": (43.0642, 141.3469),
+    "青森県": (40.8244, 140.7400),
+    "岩手県": (39.7036, 141.1527),
+    "宮城県": (38.2682, 140.8694),
+    "秋田県": (39.7186, 140.1024),
+    "山形県": (38.2404, 140.3633),
+    "福島県": (37.7503, 140.4676),
+
+    "茨城県": (36.3418, 140.4468),
+    "栃木県": (36.5657, 139.8836),
+    "群馬県": (36.3911, 139.0608),
+    "埼玉県": (35.8617, 139.6455),
+    "千葉県": (35.6074, 140.1065),
+    "東京都": (35.6895, 139.6917),
+    "神奈川県": (35.4478, 139.6425),
+
+    "新潟県": (37.9024, 139.0232),
+    "富山県": (36.6953, 137.2113),
+    "石川県": (36.5947, 136.6256),
+    "福井県": (36.0652, 136.2216),
+    "山梨県": (35.6642, 138.5684),
+    "長野県": (36.6513, 138.1810),
+    "岐阜県": (35.3912, 136.7223),
+    "静岡県": (34.9769, 138.3831),
+    "愛知県": (35.1802, 136.9066),
+
+    "三重県": (34.7303, 136.5086),
+    "滋賀県": (35.0045, 135.8686),
+    "京都府": (35.0116, 135.7681),
+    "大阪府": (34.6937, 135.5023),
+    "兵庫県": (34.6901, 135.1955),
+    "奈良県": (34.6851, 135.8048),
+    "和歌山県": (34.2260, 135.1675),
+
+    "鳥取県": (35.5039, 134.2377),
+    "島根県": (35.4723, 133.0505),
+    "岡山県": (34.6618, 133.9350),
+    "広島県": (34.3966, 132.4596),
+    "山口県": (34.1861, 131.4705),
+
+    "徳島県": (34.0658, 134.5593),
+    "香川県": (34.3401, 134.0434),
+    "愛媛県": (33.8416, 132.7661),
+    "高知県": (33.5597, 133.5311),
+
+    "福岡県": (33.5904, 130.4017),
+    "佐賀県": (33.2635, 130.3009),
+    "長崎県": (32.7448, 129.8737),
+    "熊本県": (32.7898, 130.7417),
+    "大分県": (33.2382, 131.6126),
+    "宮崎県": (31.9111, 131.4239),
+    "鹿児島県": (31.5602, 130.5581),
+    "沖縄県": (26.2124, 127.6809),
+}
+
+
+area=input("""
+どこの都道府県の天気予報が知りたいですか？
+都道府県名を書いてください(例:東京都・大阪府・北海道・愛知県...)
+
+           """)
+
+
+while area not in areas:
+    area=input("""
+都道府県名が正しく入力されていません
+どこの都道府県の天気予報が知りたいですか？
+都道府県名を書いてください(例:東京都・大阪府・北海道・愛知県...)
+
+        """)     
+if area in areas:
+    latitude,longitude=areas[area]
+
 url="https://api.open-meteo.com/v1/forecast"
-
-from datetime import datetime,timedelta
-
-latitude=35.6895
-longitude=139.6917
-
 
 headers={
     "Accept":"application/json",
@@ -70,18 +138,18 @@ weather_list={
     99:"激しいひょうを伴う雷雨"
 }
 
-
-
 def days_temp_weather():
-    x=0
-    while x<len(dates):
-        start_date=dates[x]
-        start_temp=max_temp[x]
-        start_weather=weather_date[x]
-        weather_text=weather_list.get(start_weather,"不明")
-        print(f"{start_date}日の最高気温は{start_temp}度で天気は{weather_text}です")
-        x+=1
-    for time,temp,weather  in zip(hour_time,hour_temp,hour_weather):
-        weather_text=weather_list.get(weather,"不明")
-        print(f"{time}時の気温は{temp}で天気は{weather_text}です")
+    for d_time,ma_temp,mi_temp,d_weather in zip(dates,max_temp,min_temp,weather_date):
+        weather_text_d=weather_list.get(d_weather,"不明")
+        print(f"""
+==== {d_time} ====
+最高気温:{ma_temp}°C
+最低気温:{mi_temp}°C
+天気:{weather_text_d}
+""")
+        for h_time,h_temp,h_weather in zip(hour_time,hour_temp,hour_weather):
+            date_part,hour_part=h_time.split("T")
+            if date_part==d_time:
+                    weather_text_h=weather_list.get(h_weather,"不明")
+                    print(f"{hour_part} {h_temp}°C{weather_text_h}")
 days_temp_weather()
